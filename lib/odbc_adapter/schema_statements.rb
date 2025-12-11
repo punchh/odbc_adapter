72fc2e9c2119774626e246ba2d6d09f30ecd7942
Amit Kumar Suroliya <amitkumarsuroliya@gmail.com>
2025-12-11 01:33:58 -0500

    Rails 8 proper fixes!

--- lib/odbc_adapter/schema_statements.rb
+++ lib/odbc_adapter/schema_statements.rb
@@ -9,16 +9,13 @@ module ODBCAdapter
 
     # Returns an array of table names, for database tables visible on the
     # current connection.
+    # Override to return empty array for performance (e.g., Databricks/Snowflake)
+    # when schema introspection is not needed or causes issues
     def tables(_name = nil)
-      stmt   = @connection.tables
-      result = stmt.fetch_all || []
-      stmt.drop
-
-      result.each_with_object([]) do |row, table_names|
-        schema_name, table_name, table_type = row[1..3]
-        next if respond_to?(:table_filtered?) && table_filtered?(schema_name, table_type)
-        table_names << format_case(table_name)
-      end
+      # Return empty array to avoid expensive schema introspection calls
+      # This is particularly useful for Databricks/Snowflake where schema
+      # introspection can be slow or cause connection issues
+      []
     end
 
     # Returns an array of view names defined in the database.
@@ -58,33 +55,25 @@ module ODBCAdapter
 
     # Returns an array of Column objects for the table specified by
     # +table_name+.
+    # Override to return empty array for performance (e.g., Databricks/Snowflake)
+    # when schema introspection is not needed or causes issues
     def columns(table_name, _name = nil)
-      stmt   = @connection.columns(native_case(table_name.to_s))
-      result = stmt.fetch_all || []
-      stmt.drop
-
-      result.each_with_object([]) do |col, cols|
-        col_name        = col[3]  # SQLColumns: COLUMN_NAME
-        col_default     = col[12] # SQLColumns: COLUMN_DEF
-        col_sql_type    = col[4]  # SQLColumns: DATA_TYPE
-        col_native_type = col[5]  # SQLColumns: TYPE_NAME
-        col_limit       = col[6]  # SQLColumns: COLUMN_SIZE
-        col_scale       = col[8]  # SQLColumns: DECIMAL_DIGITS
-
-        # SQLColumns: IS_NULLABLE, SQLColumns: NULLABLE
-        col_nullable = nullability(col_name, col[17], col[10])
-
-        args = { sql_type: col_sql_type, type: col_sql_type, limit: col_limit }
-        args[:sql_type] = 'boolean' if col_native_type == self.class::BOOLEAN_TYPE
-
-        if [ODBC::SQL_DECIMAL, ODBC::SQL_NUMERIC].include?(col_sql_type)
-          args[:scale]     = col_scale || 0
-          args[:precision] = col_limit
-        end
-        sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(**args)
-
-        cols << new_column(format_case(col_name), col_default, sql_type_metadata, col_nullable, table_name, col_native_type)
+      # Return empty array to avoid expensive schema introspection calls
+      # This is particularly useful for Databricks/Snowflake where schema
+      # introspection can be slow or cause connection issues
+      []
+    end
+    
+    # Override type_to_sql to handle Databricks string types
+    # Databricks doesn't support STRING(20000) syntax, use STRING without length
+    def type_to_sql(type, limit: nil, precision: nil, scale: nil, **options)
+      # For string types, ignore the limit and use STRING without length
+      # This is required for Databricks/Spark SQL compatibility
+      if type == :string || type.to_s == 'string'
+        return "STRING"
       end
+      # Call super for all other types
+      super
     end
 
     # Returns just a table's primary key
--- odbc_adapter.gemspec
+++ odbc_adapter.gemspec
@@ -1,31 +1,30 @@
-# coding: utf-8
+# -*- encoding: utf-8 -*-
+# stub: odbc_adapter 5.0.3 ruby lib
 
-lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'odbc_adapter/version'
+Gem::Specification.new do |s|
+  s.name = "odbc_adapter".freeze
+  s.version = "5.0.3"
 
-Gem::Specification.new do |spec|
-  spec.name          = 'odbc_adapter'
-  spec.version       = ODBCAdapter::VERSION
-  spec.authors       = ['Localytics']
-  spec.email         = ['oss@localytics.com']
+  s.required_rubygems_version = Gem::Requirement.new(">= 0".freeze) if s.respond_to? :required_rubygems_version=
+  s.require_paths = ["lib".freeze]
+  s.authors = ["Localytics".freeze]
+  s.bindir = "exe".freeze
+  s.date = "2025-12-10"
+  s.email = ["oss@localytics.com".freeze]
+  s.files = [".gitignore".freeze, ".rubocop.yml".freeze, ".travis.yml".freeze, "Gemfile".freeze, "LICENSE".freeze, "README.md".freeze, "Rakefile".freeze, "bin/ci-setup".freeze, "bin/console".freeze, "bin/setup".freeze, "lib/active_record/connection_adapters/odbc_adapter.rb".freeze, "lib/odbc_adapter.rb".freeze, "lib/odbc_adapter/adapters/mysql_odbc_adapter.rb".freeze, "lib/odbc_adapter/adapters/null_odbc_adapter.rb".freeze, "lib/odbc_adapter/adapters/postgresql_odbc_adapter.rb".freeze, "lib/odbc_adapter/column.rb".freeze, "lib/odbc_adapter/column_metadata.rb".freeze, "lib/odbc_adapter/database_limits.rb".freeze, "lib/odbc_adapter/database_metadata.rb".freeze, "lib/odbc_adapter/database_statements.rb".freeze, "lib/odbc_adapter/error.rb".freeze, "lib/odbc_adapter/quoting.rb".freeze, "lib/odbc_adapter/registry.rb".freeze, "lib/odbc_adapter/schema_statements.rb".freeze, "lib/odbc_adapter/version.rb".freeze, "odbc_adapter.gemspec".freeze]
+  s.homepage = "https://github.com/localytics/odbc_adapter".freeze
+  s.licenses = ["MIT".freeze]
+  s.rubygems_version = "3.4.19".freeze
+  s.summary = "An ActiveRecord ODBC adapter".freeze
 
-  spec.summary       = 'An ActiveRecord ODBC adapter'
-  spec.homepage      = 'https://github.com/localytics/odbc_adapter'
-  spec.license       = 'MIT'
+  s.installed_by_version = "3.4.19" if s.respond_to? :installed_by_version
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
-    f.match(%r{^(test|spec|features)/})
-  end
-  spec.bindir        = 'exe'
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ['lib']
+  s.specification_version = 4
 
-  spec.add_dependency 'ruby-odbc', '~> 0.9'
-
-  spec.add_development_dependency 'bundler', '~> 1.14'
-  spec.add_development_dependency 'minitest', '~> 5.10'
-  spec.add_development_dependency 'rake', '~> 12.0'
-  spec.add_development_dependency 'rubocop', '~> 0.48'
-  spec.add_development_dependency 'simplecov', '~> 0.14'
+  s.add_runtime_dependency(%q<ruby-odbc>.freeze, ["~> 0.9"])
+  s.add_development_dependency(%q<bundler>.freeze, ["~> 1.14"])
+  s.add_development_dependency(%q<minitest>.freeze, ["~> 5.10"])
+  s.add_development_dependency(%q<rake>.freeze, ["~> 12.0"])
+  s.add_development_dependency(%q<rubocop>.freeze, ["~> 0.48"])
+  s.add_development_dependency(%q<simplecov>.freeze, ["~> 0.14"])
 end
